eb6add12dc8903064ea8428ac185979619d6c48a
cloudJYJ <66666753+cloudJYJ@users.noreply.github.com>
2021-01-11 16:26:21 +0900

    LearnCSharp-01-11v2

---
+++ CSharp/1.HelloWorld/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace BrainCSharp
+{
+    class HelloWorld
+    {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                WriteLine("사용법 : HelloWorld.exe <이름>");
+                return;
+            }else if(args[0] == "연습문제")
+            {
+                WriteLine("여러분, 안녕하세요?");
+                WriteLine("반갑습니다!");
+                return;
+            }
+            WriteLine("Hello, {0}!",args[0]); // Hellow, World를 프롬프트에 출력
+        }
+    }
+}
---
+++ CSharp/2.DataTypes/DataTypes.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace DataTypes
+{
+    class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            try
+            {
+                WriteLine("[ 1 ] 정수 형식");
+                WriteLine("[ 2 ] 정수 리터럴");
+                WriteLine("[ 3 ] 오버플로우");
+                WriteLine("[ 4 ] 부동 소수점 형식");
+                WriteLine("[ 5 ] Decimal 형식");
+                WriteLine("[ 6 ] Char 형식");
+                WriteLine("[ 7 ] String 형식");
+                WriteLine("[ 8 ] 논리 형식");
+                WriteLine("[ 9 ] Object 형식");
+                WriteLine("[10 ] 박싱과 언박싱");
+                choice = int.Parse(ReadLine());
+                if (choice == 1) { IntegralTypes Integer = new IntegralTypes(); }
+                if (choice == 2) { SignedUnsigned Sign = new SignedUnsigned(); }
+                if (choice == 3) { OverFlow Over = new OverFlow(); }
+                if (choice == 4) { FloatingPoint Float = new FloatingPoint(); }
+                if (choice == 5) { Decimal Decimal = new Decimal(); }
+                if (choice == 6) { Char Char = new Char(); }
+                if (choice == 7) { String String = new String(); }
+                if (choice == 8) { Bool Bool = new Bool(); }
+                if (choice == 9) { Object Object = new Object(); }
+                if (choice == 10) { BoxingUnboxing Box = new BoxingUnboxing(); }
+                else { WriteLine("목록외의 입력으로 종료됨"); }
+            }
+            catch(Exception ex)
+            {
+                WriteLine("목록외의 입력으로 종료됨");
+            }
+
+        }
+    }
+
+    class IntegralTypes
+    {
+        public IntegralTypes() // 생성자메소드로써 해당 클래스가 인스턴스화될때 호출되어 실행됨
+        {
+            sbyte a = -10;
+            byte b = 40;
+
+            WriteLine($"a={a}, b={b}");
+
+            short c = -30000;
+            ushort d = 60000;
+
+            WriteLine($"c={c}, d={d}");
+
+            int e = -1000_0000; // 0이 7개
+            uint f = 3_0000_0000; // 0이 8개
+
+            WriteLine($"e={e}, f={f}");
+
+            long g = -5000_0000_0000; // 0이 11개
+            ulong h = 200_0000_0000_0000_0000; // 0이 18개
+
+            WriteLine($"g={g}, h={h}");
+
+            IntegerLiterals();
+        }
+        public void IntegerLiterals()
+        {
+            byte a = 240;           // 10진수 리터럴
+            WriteLine($"a={a}");
+
+            byte b = 0b1111_0000;   // 2진수 리터럴
+            WriteLine($"b={b}");
+
+            byte c = 0XF0;          // 16진수 리터럴
+            WriteLine($"c={c}");
+
+            uint d = 0x1234_abcd;   // 16진수 리터럴
+            WriteLine($"d={d}");
+        }
+    }
+    class SignedUnsigned
+    {
+        public SignedUnsigned()
+        {
+            byte a = 255;
+            sbyte b = (sbyte)a;
+
+            WriteLine(a);
+            WriteLine(b);
+        }
+    }
+    class OverFlow
+    {
+        public OverFlow()
+        {
+            uint a = uint.MaxValue;
+
+            WriteLine(a);
+
+            a++;
+
+            WriteLine(a);
+        }
+    }
+    class FloatingPoint
+    {
+        public FloatingPoint()
+        {
+            float a = 3.1415_9265_3589_7932_3846f;
+            WriteLine(a);
+
+            double b = 3.1415_9265_3589_7932_3846f;
+            WriteLine(b);
+        }
+    }
+    class Decimal
+    {
+        public Decimal()
+        {
+            float a = 3.1415_9265_3589_7932_3846_2643_3832_79f;
+            double b = 3.1415_9265_3589_7932_3846_2643_3832_79;
+            decimal c = 3.1415_9265_3589_7932_3846_2643_3832_79m;
+
+            WriteLine(a);
+            WriteLine(b);
+            WriteLine(c);
+        }
+    }
+    class Char
+    {
+        public Char()
+        {
+            char a = '안';
+            char b = '녕';
+            char c = '하';
+            char d = '세';
+            char e = '요';
+
+            Write(a);
+            Write(b);
+            Write(c);
+            Write(d);
+            Write(e);
+            WriteLine();
+        }
+    }
+    class String
+    {
+        public String()
+        {
+            string a = "안녕하세요?";
+            string b = "정용준입니다.";
+
+            WriteLine(a);
+            WriteLine(b);
+        }
+    }
+    class Bool
+    {
+        public Bool()
+        {
+            bool a = true;
+            bool b = false;
+
+            WriteLine(a);
+            WriteLine(b);
+        }
+    }
+    class Object
+    {
+        public Object()
+        {
+            object a = 123;
+            object b = 3.141592653589793238462643383279m;
+            object c = true;
+            object d = "안녕하세요";
+
+            WriteLine(a);
+            WriteLine(b);
+            WriteLine(c);
+            WriteLine(d);
+        }
+    }
+    class BoxingUnboxing
+    {
+        public BoxingUnboxing()
+        {
+            int a = 123;
+            object b = (object)a;   // a에 담긴 값을 박싱해서 힙에 저장
+            int c = (int)b;         // b에 담긴 값을 언박싱해서 스택에 저장
+
+            WriteLine(a);
+            WriteLine(b);
+            WriteLine(c);
+            double x = 3.1414213;
+            object y = x;           // x에 담긴 값을 박싱해서 힙에 저장
+            double z = (double)y;   // y에 담긴 값을 언박싱해서 스택에 저장
+
+            WriteLine(x);
+            WriteLine(y);
+            WriteLine(z);
+
+        }
+    }
+}
