b34e245972a9cea71531277656e6124caed08bc5
cloudJYJ <66666753+cloudJYJ@users.noreply.github.com>
2021-01-11 12:22:27 +0900

    LearnCSharp-01-11
    1.HelloWorld

---
+++ CSharp/1.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
